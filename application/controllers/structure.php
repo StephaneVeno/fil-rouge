8a95aec0b072c7f73880920c1a688ac48d61e1a3
StephaneVeno <stephanedu180@gmail.com>
2020-06-23 15:15:08 +0200

    Suppression après mise en accord

--- application/controllers/structure.php
+++ application/controllers/structure.php
@@ -44,30 +44,6 @@ class Structure extends CI_Controller {
     */
 
 
-
-  /*
-  * retourne la même vue, sauf que la function détermine l'ancre cotée view
-  * exemple: class/methode/$1#ancre
-  * @return string
-  */
-  public function other() { 
-    $assistance = $this->templates->display('annexes/other');
-      function service() {
-        return self::$assistance;
-      }
-      function info() {
-        return self::$assistance;
-      }
-      function aide() {
-        return self::$assistance;
-      }
-      function propos() {
-        return self::$assistance;
-      }
-  }
-
-
-
   public function plan_du_site() {
     $this->templates->display('annexes/plan_du_site');
   }
